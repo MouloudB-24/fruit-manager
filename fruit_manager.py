18e652bfe74f5b55949fab6282c39cf6fddefa95
Mouloud <mouloud.bellil@outlook.fr>
2025-09-16 10:41:28 +0200

    creation automatique des fichiers data/ si manquant

--- fruit_manager.py
+++ fruit_manager.py
@@ -1,19 +1,50 @@
 import json
+import os
 
+DATA_DIR = "data"
+PRIX = os.path.join(DATA_DIR, "prix.json")
+INVENTAIRE_PATH = os.path.join(DATA_DIR, "inventaire.json")
+TRESORERIE_PATH = os.path.join(DATA_DIR, "tresorerie.json")
 
 def ouvrir_prix(path="data/prix.json"):
+    os.makedirs(DATA_DIR, exist_ok=True)
+    if not os.path.exists(path):
+        prix_defaut = {
+            "bananes": 2,
+            "mangues": 4,
+            "ananas": 5,
+            "noix de coco": 3,
+            "papayes": 6
+        }
+        with open(path, 'w', encoding='utf-8') as fichier:
+            json.dump(prix_defaut, fichier, ensure_ascii=False, indent=4)
     with open(path, 'r', encoding='utf-8') as fichier:
         prix = json.load(fichier)
     return prix
 
 
 def ouvrir_inventaire(path="data/inventaire.json"):
+    os.makedirs(DATA_DIR, exist_ok=True)
+    if not os.path.exists(path):
+        quantite_defaut = {
+            "bananes": 200,
+            "mangues": 400,
+            "ananas": 500,
+            "noix de coco": 300,
+            "papayes": 600
+        }
+        with open(path, 'w', encoding='utf-8') as fichier:
+            json.dump(quantite_defaut, fichier, ensure_ascii=False, indent=4)
     with open(path, "r", encoding="utf-8") as fichier:
         inventaire = json.load(fichier)
     return inventaire
 
 
 def ouvrir_tresorerie(path="data/tresorerie.txt"):
+    os.makedirs(DATA_DIR, exist_ok=True)
+    if not os.path.exists(path):
+        with open(path, 'w', encoding='utf-8') as fichier:
+            json.dump(1000.0, fichier)
     with open(path, 'r', encoding='utf-8') as fichier:
         tresorerie = json.load(fichier)
     return tresorerie
